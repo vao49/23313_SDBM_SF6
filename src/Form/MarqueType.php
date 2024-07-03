b2e03f8a2cb9876ea71c1c9ea99dc12f140e572c
vao49 <fbrc.le.blanc@gmail.com>
2024-07-03 16:51:19 +0200

    Image de fond et modification de FormTypes

--- src/Form/MarqueType.php
+++ src/Form/MarqueType.php
@@ -10,6 +10,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Translation\TranslatableMessage;
+use app\Repository\FabricantRepository;
+use app\Repository\PaysRepository;
 
 
 class MarqueType extends AbstractType
@@ -18,17 +21,43 @@ class MarqueType extends AbstractType
     {
         $builder
             ->add('nomMarque', TextType::class, [
+                'label' => new TranslatableMessage('nomMarque'),
+                'label_attr' => ['class' => 'fw-bold'],
                 'attr' => ['class' => 'form-control']
             ])
+            // ->add('pays', null, [
+            //     'class' => Pays::class,
+            //     'attr' => ['class' => 'form-control']
+            // ])
+
             ->add('pays', null, [
-                'class' => Pays::class,
-                'attr' => ['class' => 'form-control']
+                'label' => new TranslatableMessage('Pays'),
+                'label_attr' => ['class' => 'fw-bold'],
+                'attr' => ['class' => 'form-control'],
+                'query_builder' => function(PaysRepository $repository) { 
+                    return $repository->createQueryBuilder('u')->orderBy('u.nomPays', 'ASC');
+                },
+                'choice_label' => function (Pays $pays): string {
+                    return $pays ? strtoupper($pays->getNomPays()) : '';
+                }
             ])
-            ->add('fabricants', null, [
-                'class' => Fabricant::class,
-                'attr' => ['class' => 'form-control']
-            ])
+
+            // ->add('fabricants', null, [
+            //     'class' => Fabricant::class,
+            //     'attr' => ['class' => 'form-control']
+            // ])
   
+            ->add('fabricants', null, [
+                'label' => new TranslatableMessage('Fabricant'),
+                'label_attr' => ['class' => 'fw-bold'],
+                'attr' => ['class' => 'form-control'],
+                'query_builder' => function(FabricantRepository $repository) { 
+                    return $repository->createQueryBuilder('u')->orderBy('u.nomFabricant', 'ASC');
+                },
+                'choice_label' => function (Fabricant $fabricant): string {
+                    return $fabricant ? strtoupper($fabricant->getNomFabricant()) : '';
+                }
+            ])
         ;
     }
 
